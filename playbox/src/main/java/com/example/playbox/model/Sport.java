ac73a86bd33f95323a50fccf44d2b827115b22ad
Pratham Gupta <prathamgupta@Prathams-MacBook-Pro.local>
2026-02-23 22:40:53 +0530

    removeddb error

--- playbox/src/main/java/com/example/playbox/model/Sport.java
+++ playbox/src/main/java/com/example/playbox/model/Sport.java
@@ -22,6 +22,6 @@ public class Sport {
     private Float pricePerHour;
     private Boolean active;
 
-    @Column(name = "ismuplislot", nullable = false, columnDefinition = "boolean default false")
+    @Column(name = "ismuplislot")
     private Boolean ismuplislot = false;
 }
